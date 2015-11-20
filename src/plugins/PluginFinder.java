c77975e7aef4511fd2a38fdecebf226d094f612c
Clément <clement.dumazy@gmail.com>
2015-11-20 17:23:26 +0100

    Adding PluginFinder and PluginFilter class.

--- src/plugins/PluginFinder.java
+++ src/plugins/PluginFinder.java
@@ -1,5 +1,20 @@
 package plugins;
 
-public class PluginFinder {
+import java.io.File;
 
+import javax.swing.Timer;
+
+public class PluginFinder {
+	protected PluginFilter filter;
+	protected File directory;
+	protected Timer timer;
+
+	public PluginFinder(File directory) {
+		this.directory = directory;
+		filter = new PluginFilter();
+	}
+
+	public String[] acceptedFiles() {
+		return directory.list(filter);
+	}
 }
