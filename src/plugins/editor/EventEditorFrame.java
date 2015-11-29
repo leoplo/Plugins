a842761e83dce5a9b6bbb8dcfab4498c6b537fd9
abxu <shyw59@gmail.com>
2015-11-30 00:08:13 +0100

    Update Editor

---
+++ src/plugins/editor/EventEditorFrame.java
@@ -0,0 +1,33 @@
+package plugins.editor;
+
+import java.io.File;
+
+import javax.swing.JMenuItem;
+
+import plugins.Plugin;
+import plugins.pluginManager.PluginFinder;
+import plugins.pluginManager.PluginObserver;
+
+@SuppressWarnings("serial")
+public class EventEditorFrame extends EditorFrame implements PluginObserver {
+
+	protected PluginFinder pluginFinder;
+	
+	public EventEditorFrame(File pluginDirectory) {
+		this.pluginFinder = new PluginFinder(pluginDirectory);
+		this.pluginFinder.registerObserver(this);
+		this.pluginFinder.startTimer();
+	}
+
+	@Override
+	public void updatePlugins(Plugin[] plugins) {
+		this.toolsMenu.removeAll();
+		
+		for(Plugin pluginToAdd : plugins) {
+			// TODO Connecter l'evenement
+			JMenuItem menuItem = new JMenuItem(pluginToAdd.getLabel());
+			this.toolsMenu.add(menuItem);
+		}
+	}
+	
+}
--- src/plugins/globalstrings/GlobalStrings.java
+++ src/plugins/globalstrings/GlobalStrings.java
@@ -1,8 +1,15 @@
 package plugins.globalstrings;
 
 public class GlobalStrings {
+	// Plugin
 	public static final String PLUGIN_LABEL_TOLOWERCASE = "To Lower Case";
 	public static final String PLUGIN_LABEL_TOUPPERCASE = "To Upper Case";
 	public static final String PLUGIN_LABEL_REMOVESVOWELS = "Removes voyels";
 	public static final String PLUGIN_LABEL_CAESARCIPHER = "Caesar cipher %d";
+	
+	// Frame
+	public static final String FRAME_TITLE = "Extendable Editor";
+	
+	// Menu
+	public static final String MENU_TOOLS = "Tools";
 }
