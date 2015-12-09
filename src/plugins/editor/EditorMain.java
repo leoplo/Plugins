e5187fd0ed75c8f23ed1943dee8c5be3268c12ba
abxu <shyw59@gmail.com>
2015-12-09 01:45:17 +0100

    Update EditorMain

--- src/plugins/editor/EditorMain.java
+++ src/plugins/editor/EditorMain.java
@@ -2,26 +2,16 @@ package plugins.editor;
 
 import java.io.File;
 
-import javax.swing.SwingUtilities;
-
 public class EditorMain {
 
 	public static String DEFAULT_DIRECTORY_LOCATION = "./dropins/plugins";
-	public static String DIRECTORY_LOCATION;
 
 	public static void main(String[] args) {
-		DIRECTORY_LOCATION = (args.length >= 1 ? args[0] : DEFAULT_DIRECTORY_LOCATION);
+		String directoryLocation = (args.length >= 1 ? args[0] : DEFAULT_DIRECTORY_LOCATION);
 
-		SwingUtilities.invokeLater(new Runnable() {
-
-			@Override
-			public void run() {
-				File pluginDirectory = new File(DIRECTORY_LOCATION);
-				EventEditorFrame eventEditorFrame = new EventEditorFrame(pluginDirectory);
-				eventEditorFrame.setVisible(true);
-			}
-
-		});
+		File pluginDirectory = new File(directoryLocation);
+		EventEditorFrame eventEditorFrame = new EventEditorFrame(pluginDirectory);
+		eventEditorFrame.setVisible(true);
 	}
 
 }
